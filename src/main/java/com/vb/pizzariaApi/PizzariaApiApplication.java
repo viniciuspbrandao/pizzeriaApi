f7003c07e8c6ade781b7dba29234f3b502d8126f
Vinicius Brandao <viniciusbrandao@vb4web.com>
2022-09-29 13:32:05 -0300

    Directory creation.

---
+++ src/main/java/com/vb/pizzariaApi/PizzariaApiApplication.java
@@ -0,0 +1,13 @@
+package com.vb.pizzariaApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzariaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzariaApiApplication.class, args);
+	}
+
+}
